9d9c709e75091aa84acc60a030b49cfae9e0d7b8
Eltayeb Hassan <ms.eme2011@gmail.com>
2018-10-04 15:19:40 +0200

    Fix Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call(MerchantTypeSeeder::class);
         $this->call(MerchantsSeeder::class);
         $this->call(services::class);
-        $this->call(AccountTypeSeeder::class);
+        //$this->call(AccountTypeSeeder::class);
         $this->call(TopUpTypeSeeder::class);//TopUpBillerSeeder
         $this->call(TopUpBillerSeeder::class);//TopUpBillerSeeder
         $this->call(TopUpSeeder::class);//TopUpBillerSeeder
