fc402c27e76854101c0ad0671d3ba112e5292418
albraa hesham <akame.pato@gmail.com>
2018-10-07 15:39:53 +0200

    Fix Problem in extends Controllers

--- app/Model/Response/E15Response.php
+++ app/Model/Response/E15Response.php
@@ -8,7 +8,7 @@ class E15Response extends Model
 {
     //
     public function E15(){
-        return $this->belongsTo('App\Model\E15');
+        return $this->belongsTo('App\Model\Payment\E15');
     }
     public function PaymentResponse(){
         return $this->belongsTo('App\Model\Response\PaymentResponse' , 'payment_response_id');
