87686b56c788357caf01d066350ec76b44e535db
albraa hesham <akame.pato@gmail.com>
2018-10-08 14:23:31 +0200

    Yet Another Commit ***K

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -31,7 +31,7 @@ class AuthController extends Controller
         if ($validator->fails()){
             return response()->json([
                 'success' => false,
-                'errors' => $validator->errors()->toJson()
+                'errors' => $validator->errors()->toArray()
             ]);
         }
 
@@ -111,7 +111,7 @@ class AuthController extends Controller
             if ($validator->fails()){
                 return response()->json([
                     'success' => false,
-                    'errors' => $validator->errors()->toJson()
+                    'errors' => $validator->errors()->toArray()
                 ]);
             }
 
@@ -166,7 +166,7 @@ class AuthController extends Controller
         if ($validator->fails()){
             return response()->json([
                 'success' => false,
-                'errors' => $validator->errors()->toJson()
+                'errors' => $validator->errors()->toArray()
             ]);
         }
 
@@ -224,7 +224,7 @@ class AuthController extends Controller
         if ($validator->fails()){
             return response()->json([
                 'success' => false,
-                'errors' => $validator->errors()->toJson()
+                'errors' => $validator->errors()->toArray()
             ]);
         }
         $phone = $request->json()->get("phone");
@@ -259,7 +259,7 @@ class AuthController extends Controller
         if ($validator->fails()){
             return response()->json([
                 'success' => false,
-                'errors' => $validator->errors()->toJson()
+                'errors' => $validator->errors()->toArray()
             ]);
         }
 
