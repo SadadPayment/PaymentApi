9d0158f5d98824a717af342910076ea0ca54333f
albraa hesham <akame.pato@gmail.com>
2018-10-09 15:27:33 +0200

    Add History Controller

--- app/Http/Controllers/API/HistoryApi.php
+++ app/Http/Controllers/API/HistoryApi.php
@@ -22,8 +22,8 @@ class HistoryApi extends Controller
 
 
         foreach ($tran as $transaction ){
-            dd($tran);
-            $transction_type = TransactionType::where('id', $tran->transaction_type)->pluck('name')->first();
+            //dd($tran);
+            $transction_type = TransactionType::where('id', $tran->transactionType())->pluck('name')->first();
             $res = Response::where("transaction_id" , $tran->id)->first();
 
             $res += ["type" => $transction_type];
