b14090fcb4fad90d0a2b98c99373e3c204a11e33
albraa hesham <akame.pato@gmail.com>
2018-10-11 15:24:12 +0200

    Test Mobile Wallet

--- routes/api.php
+++ routes/api.php
@@ -35,3 +35,4 @@ Route::post('e15_inquery','API\E15@e15_inquery');
 
 Route::get('getByUsers', 'API\ElectHistoryApiController@getByUsers');
 Route::get('getAllTransaction', 'API\HistoryApi@getAllTransactionsByUser');
+Route::get('wallet', 'API\Wallet@balance_inquiry');
